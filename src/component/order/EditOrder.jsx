e5dcce6bf71e6267b15876ffc3de60b05da377bd
Oshan Amarathunga <156085520+OshanAmarathunga@users.noreply.github.com>
2024-04-06 15:53:02 +0530

    added place order complete button

--- src/component/order/EditOrder.jsx
+++ src/component/order/EditOrder.jsx
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import axios from "axios";
 import moment from "moment";
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import './editOrder.css';
+import Swal from "sweetalert2";
 
 function EditOrder(){
     const {id}=useParams();
     const [order,setOrder]=useState(null);
     const [products,setProducts]=useState([]);
+    const navigate=useNavigate();
 
 
     useEffect(()=>{
@@ -42,13 +44,35 @@ function EditOrder(){
                         <div className="datetime">
                             Order Date : {moment(order.orderDateTime).format('MMMM Do YYYY, h:mm:ss a')}
                         </div>
-                        <div >
-                            Total Price : {order.totalPrice}
+                        <div id="txtTotalPriceLabel">
+                            Total Price : <span className="total-price">{order.totalPrice}</span> $
                         </div>
+
+                        <div><Button variant="info" onClick={()=>{
+                            Swal.fire({
+                                title: "Do you want to complete the order?",
+                                showCancelButton: true,
+                                confirmButtonText: "Yes",
+                            }).then((result) => {
+                                /* Read more about isConfirmed, isDenied below */
+                                if (result.isConfirmed) {
+                                    axios.put("http://localhost:8080/order/"+id)
+                                        .then((resp)=>{
+                                            Swal.fire("Order Completed!", "", "success");
+                                            navigate("/order");
+                                        })
+                                        .catch((e)=>{
+                                            alert(e);
+                                        })
+
+                                }
+                            });
+                        }}>Complete Order</Button>{' '}</div>
 
                     </div>
                     <div className="row my-5">
-                        <div className="col-lg-6">
+                        <div className="col-lg-6" id="orderProduct">
+                            <div>EDIT ORDER</div>
                             <Table striped bordered hover>
                                 <thead>
                                 <tr>
@@ -81,43 +105,44 @@ function EditOrder(){
                             </Table>
                         </div>
                         <div className="col-lg-6">
+                            <div>AVAILABLE PRODUCTS</div>
                             <div className="products">
-                            <Table striped bordered hover>
-                                <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Product Name</th>
-                                    <th>Add</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-
-                                {products && products.map((eachProduct)=> (
-                                    <tr key={eachProduct.id}>
-                                        <td>{eachProduct.productName}</td>
-                                        <td>{eachProduct.price}</td>
-                                        <td><Button variant="warning" onClick={()=>{
-                                            const data={
-                                                productId:eachProduct.id,
-                                                quantity:1
-                                            }
-                                            axios.post(`http://localhost:8080/order/${id}/addProducts`,data)
-                                                .then((resp)=>{
-                                                    setOrder(resp.data);
-                                                })
-                                                .catch((err)=>{
-                                                    console.log(err)
-                                                })
-                                        }}>Add</Button></td>
-
+                                <Table striped bordered hover>
+                                    <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Product Name</th>
+                                        <th>Add</th>
                                     </tr>
-                                ))
+                                    </thead>
+                                    <tbody>
 
-                                }
+                                    {products && products.map((eachProduct) => (
+                                        <tr key={eachProduct.id}>
+                                            <td>{eachProduct.productName}</td>
+                                            <td>{eachProduct.price}</td>
+                                            <td><Button variant="warning" onClick={() => {
+                                                const data = {
+                                                    productId: eachProduct.id,
+                                                    quantity: 1
+                                                }
+                                                axios.post(`http://localhost:8080/order/${id}/addProducts`, data)
+                                                    .then((resp) => {
+                                                        setOrder(resp.data);
+                                                    })
+                                                    .catch((err) => {
+                                                        console.log(err)
+                                                    })
+                                            }}>Add</Button></td>
+
+                                        </tr>
+                                    ))
+
+                                    }
 
 
-                                </tbody>
-                            </Table>
+                                    </tbody>
+                                </Table>
                             </div>
 
                         </div>
